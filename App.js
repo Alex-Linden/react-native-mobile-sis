72283292058dd98261a6657051cfab8229c9e1c2
Elise Geiger <elise.geiger@gmail.com>
2022-08-19 12:41:03 -0700

    Refactored code:
    - Changed name of StackNav to StackNavs
    - Moved Login vs Main App into different functions for rendering
    - Updated docstrings to reflect changes

--- App.js
+++ App.js
@@ -4,19 +4,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import Login from './screens/Login';
 import SisApi from './api';
 import { COLORS } from './vocabs';
 import LogoTitle from './components/LogoTitle';
-import { 
-  HomeStackScreen, 
-  LecturesStackScreen, 
-  ExercisesStackScreen, 
-  AssessmentsStackScreen, 
-  EventsStackScreen 
-} from './components/StackNav';
+import {
+  HomeStackScreen,
+  LecturesStackScreen,
+  ExercisesStackScreen,
+  AssessmentsStackScreen,
+  EventsStackScreen
+} from './components/StackNavs';
 
 const LoginStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -69,78 +69,88 @@ export default function () {
     setCohortItems(apiCohortItems);
   };
 
-  /**displays list of cohort items */
+  function renderMainApp() {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarInactiveTintColor: COLORS.mediumGrey,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Lectures') {
+              iconName = focused ? 'megaphone' : 'megaphone-outline';
+            } else if (route.name === 'Exercises') {
+              iconName = focused ? 'barbell' : 'barbell-outline';
+            } else if (route.name === 'Assessments') {
+              iconName = focused ? 'newspaper' : 'newspaper-outline';
+            } else if (route.name === 'Events') {
+              iconName = focused ? 'calendar' : 'calendar-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          headerShown: false,
+        })} >
+        <Tab.Screen name='Home'>
+          {(props) => <HomeStackScreen {...props}
+            cohortItems={cohortItems}
+            logoutUser={logoutUser} />}
+        </Tab.Screen>
+        <Tab.Screen name='Lectures'>
+          {(props) => <LecturesStackScreen {...props}
+            cohortItems={cohortItems}
+            logoutUser={logoutUser}
+          />}
+        </Tab.Screen>
+        <Tab.Screen name='Exercises'>
+          {(props) => <ExercisesStackScreen {...props}
+            cohortItems={cohortItems}
+            logoutUser={logoutUser}
+          />}
+        </Tab.Screen>
+        <Tab.Screen name='Assessments'>
+          {(props) => <AssessmentsStackScreen {...props}
+            cohortItems={cohortItems}
+            logoutUser={logoutUser}
+          />}
+        </Tab.Screen>
+        <Tab.Screen name='Events'>
+          {(props) => <EventsStackScreen {...props}
+            cohortItems={cohortItems}
+            logoutUser={logoutUser}
+          />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    );
+  }
+
+  function renderLoginScreen() {
+    return (
+      <LoginStack.Navigator
+        initialRouteName='Login'
+        screenOptions={{
+          headerTitle: (props) => <LogoTitle {...props} />,
+          headerTintColor: '#ffffff',
+          headerStyle: {
+            backgroundColor: COLORS.primary,
+          },
+        }} >
+        <LoginStack.Screen name='Login'>
+          {(props) => <Login {...props} loginUser={loginUser} />}
+        </LoginStack.Screen>
+      </LoginStack.Navigator>
+    );
+  }
+
+  /** If user is logged in, displays main app, else displays login screen */
   return (
     <NavigationContainer>
       {token
-        ? (<Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarActiveTintColor: COLORS.primary,
-            tabBarInactiveTintColor: COLORS.mediumGrey,
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === 'Home') {
-                iconName = focused ? 'home' : 'home-outline';
-              } else if (route.name === 'Lectures') {
-                iconName = focused ? 'megaphone' : 'megaphone-outline';
-              } else if (route.name === 'Exercises') {
-                iconName = focused ? 'barbell' : 'barbell-outline';
-              } else if (route.name === 'Assessments') {
-                iconName = focused ? 'newspaper' : 'newspaper-outline';
-              } else if (route.name === 'Events') {
-                iconName = focused ? 'calendar' : 'calendar-outline';
-              }
-
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            headerShown: false,
-          })} >
-          <Tab.Screen name='Home' options={{ headerShown: false }}>
-            {(props) => <HomeStackScreen {...props}
-              cohortItems={cohortItems}
-              logoutUser={logoutUser} />}
-          </Tab.Screen>
-          <Tab.Screen name='Lectures'>
-          {(props) => <LecturesStackScreen {...props}
-              cohortItems={cohortItems}
-              logoutUser={logoutUser}
-              />}
-          </Tab.Screen>
-          <Tab.Screen name='Exercises'>
-          {(props) => <ExercisesStackScreen {...props}
-              cohortItems={cohortItems}
-              logoutUser={logoutUser}
-              />}
-          </Tab.Screen>
-          <Tab.Screen name='Assessments'>
-          {(props) => <AssessmentsStackScreen {...props}
-              cohortItems={cohortItems}
-              logoutUser={logoutUser}
-              />}
-          </Tab.Screen>
-          <Tab.Screen name='Events'>
-          {(props) => <EventsStackScreen {...props}
-              cohortItems={cohortItems}
-              logoutUser={logoutUser}
-              />}
-          </Tab.Screen>
-        </Tab.Navigator>
-        )
-        : (<LoginStack.Navigator
-          initialRouteName='Login'
-          screenOptions={{
-            headerTitle: (props) => <LogoTitle {...props} />,
-            headerTintColor: '#ffffff',
-            headerStyle: {
-              backgroundColor: COLORS.primary,
-            },
-          }} >
-          <LoginStack.Screen name='Login'>
-            {(props) => <Login {...props} loginUser={loginUser} />}
-          </LoginStack.Screen>
-        </LoginStack.Navigator>
-        )
+        ? renderMainApp()
+        : renderLoginScreen()
       }
     </NavigationContainer >
   );
@@ -150,7 +160,7 @@ const styles = StyleSheet.create({
   forgot_button: {
     height: 30,
     justifyContent: 'center',
-    alignContent:'center',
+    alignContent: 'center',
     marginRight: 8,
     fontSize: 20,
     color: '#ffffff'
